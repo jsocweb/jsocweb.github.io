8b00d31f6a8764da78fda33a71113e59c9f501ac
Sachin <sachinjainkpp@gmail.com>
2023-04-23 02:05:38 -0600

    Addded credit and appendix

--- js/tatvarth.js
+++ js/tatvarth.js
@@ -27,7 +27,7 @@ $(document).ready(function () {
         case "home":
             var wurl = window.location.origin + '/tatvarth.html?gatha=' + result + '';
             var message = "*Daily Swadhyay: Tatvarth Sutra Ji* %0a%0a _*आचार्य उमास्वामी विरचित*_%0a*तत्त्वार्थसूत्र* %0a%0a *Ācārya Umāsvāmī’s*%0a*Tattvārthasūtra* %0a----------------------------------%0a%0a *प्रस्तावना* %0a%0aआचार्य उमास्‍वामी कृत मोक्षमार्ग, तत्त्वार्थ दर्शन विषयक 10 अध्‍यायों में सूत्रबद्ध ग्रन्‍थ है। कुल सूत्र 357 हैं। इसी को मोक्षशास्‍त्र भी कहते हैं। दिगम्‍बर व श्‍वेताम्‍बर पंथों दोनों को समान रूप से मान्‍य है। जैन आम्‍नाय में यह सर्व प्रधान सिद्धान्‍त ग्रन्‍थ माना जाता है। जैन दर्शन प्ररूपक होने के कारण यह जैन गीता के रूप में समझा जाता है।%0a%0aइस भावना से प्रेरित होकर की हम सभी श्रावकों को जैन धर्म के मर्म की जानकारी प्राप्त हो, हमने तत्वार्थसूत्र ग्रंथ के सूत्र को अर्थ एवं विशेषार्थ के रूप में यहाँ संयोजित करने का प्रयास किया है | हमने प्रस्तुत अर्थ, ग्रंथों से लिया है | उम्मीद है कि आप सभी लोगों को हमारी ये कोशिश पसंद आएगी एवं आप सभी अपना समय दे कर इसका लाभ उठायेंगे | इस संयोजन में यदि हमसे कोई त्रुटि हुई हो तो कृप्या आप उसे हमें अवश्य बताएँ तो हम उसे सही कर सकें | हम वीर प्रभु से प्रार्थना करते हैं की हम इस कार्य को पूरा कर सकें, इसके लिए आप सभी का सहयोग अपेक्षित है |%0a%0aContinue reading... " + wurl + "";
-            message = message.replace('<br />', '%0a').replace('<p>', '_').replace('</p>', '_');
+            message = message.replace(new RegExp("<br />", "g"), '%0a').replace(new RegExp("<p>", "g"), '_').replace(new RegExp("</p>", "g"), '_');
             newUrl = "https://wa.me/?text=" + message + ""
             $('#awhatsapp').attr("href", newUrl);
             
@@ -36,12 +36,16 @@ $(document).ready(function () {
                 // newUrl was empty string, false, 0, null, undefined, ...
                 var wurl = window.location.origin + '/tatvarth.html?gatha=' + result + '';
                 var message = "*Daily Swadhyay: Tatvarth Sutra Ji* %0a%0a _*मंगलाचरण*_%0a----------------------------------%0a%0a *मोक्ष-मार्गस्य नेतारं, भेत्तारं कर्म-भूभृताम्‌।%0aज्ञातारं विश्व - तत्त्वानां, वन्दे तद्गुण - लब्धये॥* %0a%0a *अर्थ:* जो मोक्षमार्ग का प्रवर्तक है, कर्मरूपी पर्वतों का भेदन करने वाला है और समस्त तत्त्वों को जानता है, उसे मैं उन गुणों की प्राप्ति के लिए नमस्कार करता हूँ।%0a%0a *Meaning:* I make obeisance to the Omniscient Lord – the promulgator of the path to liberation, the destroyer of the mountains of karmas, and the knower of the whole of reality – so that I may realize these qualities.%0a%0aContinue reading... " + wurl + "";
-                message = message.replace('<br />', '%0a').replace('<p>', '_').replace('</p>', '_');
+                message = message.replace(new RegExp("<br />", "g"), '%0a').replace(new RegExp("<p>", "g"), '_').replace(new RegExp("</p>", "g"), '_');
                 newUrl = "https://wa.me/?text=" + message + ""
                 $('#awhatsapp').attr("href", newUrl);
             }
 
         case "credits":
+            //$('#awhatsapp').hide();
+            loadContent('tatvarth/' + result + '.html', '#' + result);
+            break;
+        case "appendix":
             //$('#awhatsapp').hide();
             loadContent('tatvarth/' + result + '.html', '#' + result);
             break;
@@ -59,15 +63,17 @@ $(document).ready(function () {
                 $('#audiosrc').html('<audio controls controlsList="nodownload"><source src="tatvarth/audio/' + dt.audiosrc + '" type="audio/mpeg"></audio><br />उच्चारण - आर्यिका 105 पूर्णमती माता जी के मधुर स्वर में।');
                 $('#arth').html('<b><font color=darkmagenta>अर्थ : </font></b>' + dt.arth);
                 $('#meaning').html('<b><font color=darkmagenta>Meaning : </font></b>' + dt.meaning);
-                $('#vishesharth').html('<p class=\"paragraph\">' + dt.vishesharth + '</p>');
+                $('#vyakhya').html('<p class=\"paragraph\">' + dt.vyakhya + '</p>');
                 $('#explanation').html('<p class=\"paragraphE\">' + dt.explanation + '</p>');
                 $.each(dt.vidsrc, function (index, value) {
                     $('#vidsrc').append(value);
                 });
 
                 var wurl = window.location.origin + '/tatvarth.html?gatha=' + result + '';
-                var message = "*Daily Swadhyay: Tatvarth Sutra Ji* %0a%0a _*" + dt.chapter + "*_%0a----------------------------------%0a%0a" + dt.title + "%0a*सूत्र: " + dt.sutra + "*%0a%0a*अर्थ:* " + dt.arth + "%0a%0a*Meaning:* " + dt.meaning + "%0a%0aContinue reading... " + wurl + "";
-                message = message.replace('<br />', '%0a').replace('<p>', '_').replace('</p>', '_');
+                var message = "*Daily Swadhyay: Shri Tatvarth Sutra* %0a%0a _*" + dt.chapter + "*_%0a----------------------------------%0a%0a" + dt.title + "%0a*सूत्र:* " + dt.sutra + "%0a%0a*अर्थ:* " + dt.arth + "%0a%0a*Meaning:* " + dt.meaning + "%0a%0aContinue reading... " + wurl + "";
+                console.log(message);
+                message = message.replace(new RegExp("<br />", "g"), '%0a').replace(new RegExp("<p>", "g"), '_').replace(new RegExp("</p>", "g") , '_');
+                console.log(message);
                 newUrl = "https://wa.me/?text=" + message +""
                 $('#awhatsapp').show();
                 $('#awhatsapp').attr("href", newUrl);
